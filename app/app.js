fbf8240c902cf9563b51261bf200257ea2df55ed
Jeimison Moreno <jeimison.ti@gmail.com>
2020-04-03 09:01:10 -0300

    Mensagem de reload inserida e scripts extras removidos

--- app/app.js
+++ app/app.js
@@ -1,3 +1,7 @@
 function saveCode(){
     alert('Salvamento ainda não disponível.');
 }
+
+window.onbeforeunload = function() {
+    return "É possível que as alterações feitas não sejam salvas.";
+}
